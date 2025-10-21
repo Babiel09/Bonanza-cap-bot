3362cda2d523be7181220a0dd4dad2032c7a1d40
Gabriel Castro <estudosbabiel@gmail.com>
2025-10-21 13:48:04 -0300

    config: Created Bot Imovel DTO

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
+import { ImovelDTO } from './DTO/bot.imovel.dto';
 
 @Module({
     imports:[],
     controllers:[BotController],
-    providers:[BotService]
+    providers:[BotService,ImovelDTO]
 })
 export class BotModule {}
