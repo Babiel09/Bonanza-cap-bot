0b1083d140e1f5e58eae2833764dd0b9b2dc6227
Gabriel Castro <estudosbabiel@gmail.com>
2025-10-21 13:59:30 -0300

    feat: Imovel DTO finished

--- src/bot/DTO/bot.imovel.dto.ts
+++ src/bot/DTO/bot.imovel.dto.ts
@@ -1,5 +1,19 @@
 import { Injectable } from "@nestjs/common";
+import { IsString } from "class-validator";
 
 @Injectable()
 
-export class ImovelDTO{}
+export class ImovelDTO{
+
+    //Exemplo de URL:
+//https://mg.olx.com.br/belo-horizonte-e-regiao/celulares/iphone-xr-1446728594?lis=listing_no_category
+
+    @IsString()
+    regiao:string;
+
+    @IsString()
+    categoria:string;
+
+    @IsString()
+    nomeDoImovel:string;
+}
